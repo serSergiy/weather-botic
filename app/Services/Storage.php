0efbe9a2680532c08f07a40b02b86e5aa9cb79cc
Serhii Zashchik <zachik13@gmail.com>
2019-12-23 03:21:33 +0200

    [WTH-3] Added local storage

---
+++ app/Services/Storage.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+class Storage
+{
+    public const STORAGE_DIR = '/data/';
+
+    public function store(string $chatId, array $data): void
+    {
+        file_put_contents(base_path() . self::STORAGE_DIR . $chatId . '.json', json_encode($data));
+    }
+
+    public function get(string $chatId): array
+    {
+        $fileName = base_path() . self::STORAGE_DIR . $chatId . '.json';
+        if (is_file($fileName)){
+            return json_decode(file_get_contents(base_path() . self::STORAGE_DIR . $chatId . '.json'));
+        }
+        return [];
+    }
+
+    public function getLastCommand(string $chatId): string
+    {
+        $fileName = base_path() . self::STORAGE_DIR . $chatId . '.json';
+        if (is_file($fileName)){
+            $data = json_decode(file_get_contents(base_path() . self::STORAGE_DIR . $chatId . '.json'));
+            return $data['last_command'] ?? '';
+        }
+        return '';
+    }
+}
